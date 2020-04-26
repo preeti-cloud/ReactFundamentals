16d915dd04451faef269230c30aa0d3c1a212af2
preeti-cloud <preeti18yd@gmail.com>
2020-04-26 21:15:02 +0530

    3 files modified dynamic styling

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
-import './style.css';
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
+  state={
+    count:0
+  };
+
+formatCount(){
+  return this. state. count ===0? " Zero" :
+this. state. count ;
+}
+
+dynamicStyling(){
+  let classes ="badge badge-";
+  classes+=this. state. count===0? "warning" : "primary";
+  return classes ;
   }
 
   render() {
@@ -17,16 +26,24 @@ class App extends Component {
         <h1>React Fundamentals</h1>
 <div> 
     <h3 className="header">
-    Dunamic state change 
+    Dynamic state change 
     </h3>
+
+    The current state counter value is 
+    <span 
+    className={this.dynamicStyling()}>
+    {this. formatCount()}
+    </span >
+    <br></br>
+    <button className ="btn btn-secondary"
+    onClick=""> 
+    Increment
+    </button>
 </div>
 
 
 
 
-        <p>
-          Start editing to see some magi
-        </p>
       </div>
     );
   }
